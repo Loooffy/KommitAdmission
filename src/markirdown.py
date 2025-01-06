2bb97535a38fbf8c6e8351dcabe2287dda46ccc3
Loooffy <Loooffy>
2025-01-06 10:09:59 +0800

    Organize folder sreucture.

--- src/markirdown.py
+++
@@ -1,7 +0,0 @@
-from markitdown import MarkItDown
-
-class MarkdownConverter:
-    def convert_markdown(self, input_file_name):
-        md = MarkItDown()
-        result = md.convert(input_file_name)
-        return result
